5ffcc6f8272a3bcef8d51ab9db3a77b9190bf8a7
Anusha <katika.anusha@gmail.com>
2020-05-20 00:29:47 -0400

    Heroku changes

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const mongoose = require('mongoose');
 const cors =require('cors');
 
 const app = express();
+const port = process.env.PORT || 4000;
 
 //allow cross-origin requests
 app.use(cors());
@@ -13,6 +14,7 @@ mongoose.connection.once('open', ()=>{
   console.log("connected to database")
 })
 app.use('/graphql',graphqlHTTP({schema, graphiql:true}));
-app.listen(4000,()=>{
+app.listen(port,()=>{
   console.log("now listening for requests on port 4000");
 })
+  
